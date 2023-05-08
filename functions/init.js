15c7d70fea98a4338d1f55b4f67293837581fa9f
TurtleY0da <timothy.veugelers@gmail.com>
2023-05-07 20:44:22 -0600

    Testing ENV variables

---
+++ functions/init.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+
+exports.handler = async function () {
+  let response = await fetch(
+    `https://testingdomain.metered.live/api/v1/turn/credentials?apiKey=${process.env.METERED_API_KEY}`
+  );
+  let iceServers = await response.json();
+  return {
+    statusCode: 200,
+    body: JSON.stringify(iceServers),
+  };
+};
