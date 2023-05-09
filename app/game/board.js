c9c48b789a2f3112f1adc81b3de19660167e21a4
codingcolonel <106043035+codingcolonel@users.noreply.github.com>
2023-05-09 14:47:34 -0600

    added fullscreen toggle functionality

--- app/game/board.js
+++ app/game/board.js
@@ -3,42 +3,72 @@
 let cnv = document.querySelector('canvas');
 let ctx = cnv.getContext('2d');
 let scale = window.devicePixelRatio;
-let trueHeight = Math.floor(screen.height * scale);
-let trueWidth = Math.floor(screen.width * scale);
-let centerWidth = trueWidth / 2;
-let centerHeight = trueHeight / 2;
+let trueHeight = Math.floor(window.innerHeight * scale);
+let trueWidth = Math.floor(window.innerWidth * scale);
 cnv.height = trueHeight;
 cnv.width = trueWidth;
 
-// Draw Backround
-ctx.fillStyle = 'Navy';
-ctx.fillRect(0, 0, trueWidth, trueHeight);
+// Testing listener
+document.addEventListener('click', fullscreenToggle);
 
-// Draw defending board
-ctx.fillStyle = 'White';
-ctx.fillRect(
-  centerWidth - 150 - trueWidth * 0.4,
-  centerHeight - trueHeight * 0.25,
-  trueWidth * 0.33,
-  trueHeight * 0.85
-);
+function fullscreenToggle() {
+  // Change width and height when switching in/out of fullscreen
+  if (!document.fullscreenElement) {
+    document.documentElement.requestFullscreen();
+    trueHeight = Math.floor(screen.height * scale);
+    trueWidth = Math.floor(screen.width * scale);
+  } else if (document.exitFullscreen) {
+    document.exitFullscreen();
+    trueHeight = Math.floor(window.innerHeight * scale);
+    trueWidth = Math.floor(window.innerWidth * scale);
+  }
+  cnv.height = trueHeight;
+  cnv.width = trueWidth;
+  console.log(trueHeight);
+  console.log(trueWidth);
+  drawBoard();
+}
 
-// Draw attacking board
-ctx.fillStyle = 'White';
-ctx.fillRect(
-  centerWidth + 150,
-  centerHeight - trueHeight * 0.25,
-  trueWidth * 0.33,
-  trueHeight * 0.85
-);
+function drawBoard() {
+  // Update center values for drawing
+  let centerWidth = trueWidth / 2;
+  let centerHeight = trueHeight / 2;
 
-// Draw boxes
-// ctx.strokeStyle = 'skyblue';
-// ctx.lineWidth = 2;
-// for (let i = 180; i < 470; i += 30) {
-//   for (let j = 30; j <= 600; j += 30) {
-//     ctx.strokeRect(i, j, 30, 30);
-//   }
-// }
+  // Draw Background
+  ctx.fillStyle = 'Navy';
+  ctx.fillRect(0, 0, trueWidth, trueHeight);
+
+  // Draw defending board
+  ctx.fillStyle = 'White';
+  ctx.fillRect(
+    centerWidth - trueWidth * 0.025 - trueWidth * 0.45,
+    centerHeight - trueWidth * 0.225,
+    trueWidth * 0.45,
+    trueWidth * 0.45
+  );
+
+  // Draw attacking board
+  ctx.fillStyle = 'White';
+  ctx.fillRect(
+    centerWidth + trueWidth * 0.025,
+    centerHeight - trueWidth * 0.225,
+    trueWidth * 0.45,
+    trueWidth * 0.45
+  );
+
+  // Test line
+  ctx.fillStyle = 'red';
+  ctx.fillRect(0, centerHeight, trueWidth, 1);
+
+  // For later
+  // Draw boxes
+  // ctx.strokeStyle = 'skyblue';
+  // ctx.lineWidth = 2;
+  // for (let i = 180; i < 470; i += 30) {
+  //   for (let j = 30; j <= 600; j += 30) {
+  //     ctx.strokeRect(i, j, 30, 30);
+  //   }
+  // }
+}
 
 // http://en.battleship-game.org/
