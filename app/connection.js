65e3bb5f317c57c5af1af9f3ccb7cca309c4f7bd
TurtleY0da <timothy.veugelers@gmail.com>
2023-05-10 22:26:37 -0600

    added testing window export

--- app/connection.js
+++ app/connection.js
@@ -74,7 +74,6 @@ connection.onwaiting = async function () {
   await channel.subscribe("offer", async function (msg) {
     console.log(msg);
   });
-  console.log(channel);
 
   codecrypt.generateAuthenticator();
   codeOut.innerHTML = codecrypt.authenticator;
@@ -168,3 +167,4 @@ function newMessage(msg) {
 }
 
 window.connection = connection;
+window.channel = channel;
