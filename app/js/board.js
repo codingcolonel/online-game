6146c2cadc1d4b9e13d16f21b87f9e2bb56cb126
codingcolonel <106043035+codingcolonel@users.noreply.github.com>
2023-05-25 14:48:30 -0600

    added reset, randomize and confirm button for later

--- app/js/board.js
+++ app/js/board.js
@@ -5,14 +5,14 @@ import {
   checkArrayPosition,
   findTileByCoordinates,
   updateShips,
-} from "./functions.js";
-import { playerShips, opponentShips } from "./ship.js";
+} from './functions.js';
+import { playerShips, opponentShips } from './ship.js';
 
 // Set up canvas and 2d graphics content
 /** @type {HTMLCanvasElement} */
-let cnv = document.getElementById("mainCanvas");
+let cnv = document.getElementById('mainCanvas');
 /** @type {CanvasRenderingContext2D} */
-let ctx = cnv.getContext("2d");
+let ctx = cnv.getContext('2d');
 let scale = window.devicePixelRatio;
 let TrueHeight = Math.floor(window.innerHeight * scale);
 let TrueWidth = Math.floor(window.innerWidth * scale);
@@ -24,7 +24,13 @@ let defendingTiles = [];
 let attackingTiles = [];
 
 // Board info variables
-let defendingBoard, attackingBoard;
+let defendingBoard,
+  attackingBoard,
+  buttons,
+  resetButton,
+  randomizeButton,
+  confirmationButton,
+  tileLength;
 
 // Draw the board on load
 window.onload = function () {
@@ -55,14 +61,43 @@ function drawBoard(reset) {
     sideLength: Math.round(TrueWidth * 0.4 * 10) / 10, // length is same as height (40% of screen)
   };
 
+  // Update button objects
+  tileLength = defendingBoard.sideLength / 10;
+
+  // For all three buttons
+  buttons = {
+    length: Math.round(tileLength * 3 * 10) / 10, // length is 3 tiles wide
+    height: Math.round(tileLength * 10) / 10, // height is 1 tile
+  };
+
+  // if (ship placing phase)
+  resetButton = {
+    x: defendingBoard.x, // x is same as defending board
+    y: Math.round((defendingBoard.y + tileLength * 10.5) * 10) / 10, // y is placed half a tile below the board
+    colour: 'navy',
+  };
+  randomizeButton = {
+    x: Math.round((defendingBoard.x + tileLength * 3.5) * 10) / 10, // x is offset by 3.5 tiles to the right
+    y: resetButton.y, // y is is the same as reset button
+    colour: 'navy',
+  };
+
+  confirmationButton = {
+    x: Math.round((defendingBoard.x + tileLength * 7) * 10) / 10, // x is offset by 7 tiles to the right
+    y: resetButton.y, // y is is the same as reset button
+    colour: 'green',
+  };
+  // else
+  // do nothing
+
   // Draw Background
   ctx.clearRect(0, 0, TrueWidth, TrueHeight);
 
   // Draw defending board
-  singleBoard(defendingBoard, defendingTiles, "Navy", reset);
+  singleBoard(defendingBoard, defendingTiles, 'Navy', reset);
 
   // Draw attacking board
-  singleBoard(attackingBoard, attackingTiles, "Red", reset);
+  singleBoard(attackingBoard, attackingTiles, 'Red', reset);
 
   // Update canvas
   updateCanvas();
@@ -80,14 +115,14 @@ function drawBoard(reset) {
  */
 function singleBoard(board, tiles, colour, resetArrays) {
   // Draw board
-  ctx.fillStyle = "white";
+  ctx.fillStyle = 'white';
   ctx.fillRect(board.x, board.y, board.sideLength, board.sideLength);
 
   // Variables for drawing letters and numbers
   let counter = 1;
-  let letter = "A";
+  let letter = 'A';
 
-  ctx.strokeStyle = "black";
+  ctx.strokeStyle = 'black';
   ctx.lineWidth = 2;
   let currentIndex = 0;
   for (
@@ -96,9 +131,9 @@ function singleBoard(board, tiles, colour, resetArrays) {
     i += board.sideLength / 10
   ) {
     // Draw numbers
-    ctx.font = "25px Verdana, sans-serif";
-    ctx.fillStyle = "#ffffff";
-    ctx.textAlign = "center";
+    ctx.font = `${tileLength * 0.5}px Verdana, sans-serif`;
+    ctx.fillStyle = '#ffffff';
+    ctx.textAlign = 'center';
     ctx.fillText(
       `${counter}`,
       i + board.sideLength / 20,
@@ -116,7 +151,7 @@ function singleBoard(board, tiles, colour, resetArrays) {
       // Reset arrays if parameter is true
       if (resetArrays === true) {
         if (tiles.length < 100) {
-          tiles.push(addTileToArray(i, j, "none"));
+          tiles.push(addTileToArray(i, j, 'none'));
         }
       } else {
         let state = tiles[currentIndex].state;
@@ -124,10 +159,10 @@ function singleBoard(board, tiles, colour, resetArrays) {
       }
       currentIndex++;
 
-      if (letter !== "END") {
+      if (letter !== 'END') {
         // Draw letters
-        ctx.font = "25px Verdana, sans-serif";
-        ctx.fillStyle = "#ffffff";
+        ctx.font = `${tileLength * 0.5}px Verdana, sans-serif`;
+        ctx.fillStyle = '#ffffff';
         ctx.fillText(
           `${letter}`,
           board.x - board.sideLength / 25,
@@ -142,6 +177,55 @@ function singleBoard(board, tiles, colour, resetArrays) {
   ctx.strokeStyle = colour;
   ctx.lineWidth = 5;
   ctx.strokeRect(board.x, board.y, board.sideLength, board.sideLength);
+
+  // Draw reset button
+  ctx.fillStyle = resetButton.colour;
+  ctx.fillRect(resetButton.x, resetButton.y, buttons.length, buttons.height);
+
+  // Draw reset button
+  ctx.fillStyle = randomizeButton.colour;
+  ctx.fillRect(
+    randomizeButton.x,
+    randomizeButton.y,
+    buttons.length,
+    buttons.height
+  );
+
+  // Draw reset button
+  ctx.fillStyle = confirmationButton.colour;
+  ctx.fillRect(
+    confirmationButton.x,
+    confirmationButton.y,
+    buttons.length,
+    buttons.height
+  );
+
+  // Reset Text
+  ctx.font = `${tileLength * 0.5}px Verdana, sans-serif`;
+  ctx.fillStyle = '#ffffff';
+  ctx.fillText(
+    'RESET',
+    resetButton.x + buttons.length * 0.5,
+    resetButton.y + tileLength * 0.7
+  );
+
+  // Random Text
+  ctx.font = `${tileLength * 0.5}px Verdana, sans-serif`;
+  ctx.fillStyle = '#ffffff';
+  ctx.fillText(
+    'RANDOM',
+    randomizeButton.x + buttons.length * 0.5,
+    randomizeButton.y + tileLength * 0.7
+  );
+
+  // Confirm Text
+  ctx.font = `${tileLength * 0.5}px Verdana, sans-serif`;
+  ctx.fillStyle = '#ffffff';
+  ctx.fillText(
+    'CONFIRM',
+    confirmationButton.x + buttons.length * 0.5,
+    confirmationButton.y + tileLength * 0.7
+  );
 }
 
 function updateCanvas() {
@@ -160,19 +244,19 @@ function updateCanvas() {
       centerY: element.y + defendingBoard.sideLength / 20,
     };
     switch (element.state) {
-      case "none":
+      case 'none':
         drawBlank(tile);
         break;
-      case "miss":
+      case 'miss':
         drawMiss(tile);
         break;
-      case "ship":
+      case 'ship':
         drawShip(tile);
         break;
-      case "hover":
+      case 'hover':
         drawHover(tile);
         break;
-      case "shiphit":
+      case 'shiphit':
         // drawShip(tile)
         // drawX('red', tile)
         break;
@@ -190,27 +274,27 @@ function updateCanvas() {
       centerX: element.x + attackingBoard.sideLength / 20,
       centerY: element.y + attackingBoard.sideLength / 20,
     };
-    if (element.state === "miss") {
+    if (element.state === 'miss') {
       // Draw dot to mark as a miss
       drawMiss(tile);
-    } else if (element.state === "hit") {
+    } else if (element.state === 'hit') {
       // Draw red x to mark as hit
-      drawX("red", tile);
-    } else if (element.state === "sunk") {
-      drawX("black", tile);
+      drawX('red', tile);
+    } else if (element.state === 'sunk') {
+      drawX('black', tile);
     }
   }
 }
 
 function drawBlank(tile) {
-  ctx.fillStyle = "white";
+  ctx.fillStyle = 'white';
   ctx.fillRect(
     tile.x1,
     tile.y1,
     attackingBoard.sideLength / 10,
     attackingBoard.sideLength / 10
   );
-  ctx.strokeStyle = "black";
+  ctx.strokeStyle = 'black';
   ctx.lineWidth = 2;
   ctx.strokeRect(
     tile.x1,
@@ -244,14 +328,14 @@ function drawX(color, tile) {
 }
 
 function drawMiss(tile) {
-  ctx.fillStyle = "black";
+  ctx.fillStyle = 'black';
   ctx.beginPath();
   ctx.arc(tile.centerX, tile.centerY, 5, 0, 2 * Math.PI);
   ctx.fill();
 }
 
 function drawShip(tile) {
-  ctx.fillStyle = "blue";
+  ctx.fillStyle = 'blue';
   ctx.fillRect(
     tile.x1,
     tile.y1,
@@ -262,7 +346,7 @@ function drawShip(tile) {
 }
 
 function drawHover(tile) {
-  ctx.fillStyle = "white";
+  ctx.fillStyle = 'white';
   ctx.fillRect(
     tile.x1,
     tile.y1,
@@ -270,7 +354,7 @@ function drawHover(tile) {
     defendingBoard.sideLength / 10
   );
 
-  ctx.fillStyle = "rgba(0, 0, 255, 0.3)";
+  ctx.fillStyle = 'rgba(0, 0, 255, 0.3)';
   ctx.fillRect(
     tile.x1,
     tile.y1,
@@ -300,6 +384,10 @@ export {
   cnv,
   trueWidth,
   trueHeight,
+  resetButton,
+  randomizeButton,
+  confirmationButton,
+  buttons,
 };
 
 // http://en.battleship-game.org/
