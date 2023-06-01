440e554a59788f253dfbd6fe027e05e18655094f
codingcolonel <106043035+codingcolonel@users.noreply.github.com>
2023-06-01 14:31:20 -0600

    wrong slash direction

--- app/js/board.js
+++ app/js/board.js
@@ -66,7 +66,7 @@ function generateSource(source) {
 window.onload = function () {
   drawBoard(true);
   logger.generic(
-    "Click on a ship to move it to a new location/nDouble click a ship to rotate it"
+    "Click on a ship to move it to a new location\nDouble click a ship to rotate it"
   );
 };
 
